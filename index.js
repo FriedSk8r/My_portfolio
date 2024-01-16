1871340317a5908f75add5293c534100441e67f2
vakhtang khelashvili <115697470+FriedSk8r@users.noreply.github.com>
2024-01-16 12:31:31 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
-    const navLinks = document.querySelectorAll('.nav__link')
     const navToggle = document.querySelector('.nav-toggle');
     const nav = document.querySelector('.nav');
+    const navLinks = document.querySelectorAll('.nav__link');
 
     navToggle.addEventListener('click', function () {
         nav.classList.toggle('nav-open');
+    });
+
+    // Close the navigation menu when a nav link is clicked
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            nav.classList.remove('nav-open');
+        });
     });
 });
