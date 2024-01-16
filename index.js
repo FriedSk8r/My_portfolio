acbc73b4b5e91f51c8fe0b3d232a8ad2b67f9470
vakhtang khelashvili <115697470+FriedSk8r@users.noreply.github.com>
2024-01-16 13:16:06 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const navToggle = document.querySelector('.nav-toggle');
-    const nav = document.querySelector('.nav');
-    const navLinks = document.querySelectorAll('.nav__link');
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
 
-    navToggle.addEventListener('click', function () {
-        nav.classList.toggle('nav-open');
-    });
-
-    // Close the navigation menu when a nav link is clicked
-    navLinks.forEach(function (link) {
-        link.addEventListener('click', function () {
-            nav.classList.remove('nav-open');
-        });
-    });
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
 });
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
