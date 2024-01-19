76c4b60e619eed58bd0f467af3ef1f306e317874
vakhtang khelashvili <115697470+FriedSk8r@users.noreply.github.com>
2024-01-19 10:08:45 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,41 @@ function toggleMenu() {
     navbarLinks.classList.toggle('active');
     menuIcon.classList.toggle('active');
 }
+
+// Add click event listener to portfolio items
+const clickableItems = document.querySelectorAll('.clickable');
+clickableItems.forEach(item => {
+    item.addEventListener('click', () => {
+        const url = item.getAttribute('data-url');
+        if (url) {
+            window.location.href = url;
+        }
+    });
+});
+
+// Add click event listener to the Home link
+const homeLink = document.querySelector('.nav__link a[href="#home"]');
+homeLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.location.href = 'index.html#home';
+});
+
+// Add click event listeners to other links
+const servicesLink = document.querySelector('.nav__link a[href="#services"]');
+const aboutLink = document.querySelector('.nav__link a[href="#about"]');
+const workLink = document.querySelector('.nav__link a[href="#work"]');
+
+servicesLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.location.href = 'index.html#services';
+});
+
+aboutLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.location.href = 'index.html#about';
+});
+
+workLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.location.href = 'index.html#work';
+});
